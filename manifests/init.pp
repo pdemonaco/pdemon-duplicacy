ece8b23305105a5a0ae1a38420259e316302d27b
Phil DeMonaco <pdemon@gmail.com>
2018-09-08 21:08:56 -0400

    Lookups & Real Deploys
    Added the lookup rules for the various datatypes used by this
    repository. Additionally, this revision actually creates the target
    repos and is hopefully ready for some real-world testing in my
    environment.

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,13 +12,21 @@
 # @param mail_package_name [Array[String]]
 #   Package or list of packages needed to send email from this host.
 #
+# @param local_repos [Array[String]]
+#   List of repo names which should be deployed on this machine. Note that
+#   each repo must be defined in the `repos` parameter.
+#
 # @param repos [Hash]
 #   A Hash where the keys are the names of the repositories to be deployed on
-#   this system are stored.
+#   this system are stored. This is a very deep structure.
 class duplicacy (
   Array[String] $package_name,
   Array[String] $mail_package_name,
-  Hash[String, Hash[String, Variant[String, Hash[String, String]]]] $repos = {},
+  Array[String] $local_repos,
+  Hash[String, Hash[String, Variant[String, Hash[String, Variant[String,
+  Hash[String, Variant[String, Hash[String, Variant[String, Integer]]]]]],
+  Array[String], Array[Hash[String, Variant[String, Integer, Hash[String,
+  Variant[String, Integer]]]]]]]] $repos = {},
 ) {
   # Ensure the duplicacy package is present
   package { $duplicacy::package_name:
@@ -31,5 +39,13 @@ class duplicacy (
   }
 
   # Create repositories
-
+  $local_repos.each | $repo_id | {
+    if $repo_id in $repos {
+      duplicacy::repository { $repo_id:
+        * => $repos[$repo_id],
+      }
+    } else {
+      notify { "Skipping undefined repo: ${repo_id}": }
+    }
+  }
 }
--- spec/classes/duplicacy_spec.rb
+++ spec/classes/duplicacy_spec.rb
@@ -18,10 +18,61 @@ describe 'duplicacy' do
     }
   end
 
+  let(:params) do
+    {
+      'local_repos' => ['my-repo'],
+      'repos' => {
+        'my-repo' => {
+          'repo_path' => '/my/backup/dir',
+          'user' => 'root',
+          'storage_targets' => {
+            'default' => {
+              'target' => {
+                'url' => 'b2://my-bucket',
+                'b2_id' => 'my-id',
+                'b2_app_key' => 'my-key',
+              },
+              'encryption' => {
+                'password' => 'batman',
+              },
+            },
+          },
+          'filter_rules' => [
+            '+foo/baz/*',
+            '-*',
+          ],
+          'backup_schedules' => [
+            {
+              'storage_name' => 'default',
+              'cron_entry' => {
+                'hour' => '1',
+                'minute' => '30',
+              },
+              'threads' => 8,
+              'email_recipient' => 'me@example.com',
+            },
+          ],
+        },
+      },
+    }
+  end
+
   context 'Compiles' do
     it { is_expected.to compile }
 
     it { is_expected.to contain_package('app-backup/duplicacy-bin') }
     it { is_expected.to contain_package('mail-client/mutt') }
+  end
+
+  context 'Skipping undefined repo' do
+    let(:params) do
+      super().merge('local_repos' => ['missing-repo'])
+    end
+
+    it { is_expected.to contain_notify('Skipping undefined repo: missing-repo') }
+  end
+
+  context 'One real repo' do
+    it { is_expected.to contain_duplicacy__repository('my-repo') }
   end
 end
