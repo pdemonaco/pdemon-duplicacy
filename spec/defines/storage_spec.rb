702c74a21482b7131dfd03b43473cd1d33d70398
Phil DeMonaco <pdemon@gmail.com>
2018-09-23 15:07:16 -0400

    Storage Testing Cleanup
    Reduced redundant tests to more complicated single tests on individual
    resources.

--- spec/defines/storage_spec.rb
+++ spec/defines/storage_spec.rb
@@ -143,38 +143,28 @@ describe 'duplicacy::storage' do
     it { is_expected.to have_file_resource_count(1) }
     it {
       is_expected.to contain_file('env_my-repo_other_bucket').with(
-        'ensure' => 'file',
-        'path' => '/my/super/safe/data/.duplicacy/puppet/scripts/other_bucket.env',
-      )
-    }
-    it {
-      is_expected.to contain_file('env_my-repo_other_bucket').with_content(
-        [
+        ensure: 'file',
+        path: '/my/super/safe/data/.duplicacy/puppet/scripts/other_bucket.env',
+        content: [
           '#!/bin/sh
 # Export B2 Parameters
 export DUPLICACY_OTHER_BUCKET_B2_ID="my-id"
 export DUPLICACY_OTHER_BUCKET_B2_KEY="my-app-key"
 ',
         ],
+        owner: 'me',
+        group: 'me',
+        mode: '0600',
       )
     }
-    it { is_expected.to contain_file('env_my-repo_other_bucket').with_owner('me') }
-    it { is_expected.to contain_file('env_my-repo_other_bucket').with_group('me') }
-    it { is_expected.to contain_file('env_my-repo_other_bucket').with_mode('0600') }
 
     it {
-      is_expected.to contain_exec('add_my-repo_other_bucket').with_onlyif(
-        [
+      is_expected.to contain_exec('add_my-repo_other_bucket').with(
+        onlyif: [
           'test -f /my/super/safe/data/.duplicacy/preferences',
           'test 0 -eq $(sed -e \'s/"//g\' /my/super/safe/data/duplicacy/preferences | awk \'/name/ {print $2}\' | grep other_bucket | wc -l)',
         ],
-      )
-    }
-
-    # Validate the environment
-    it {
-      is_expected.to contain_exec('add_my-repo_other_bucket').with_environment(
-        [
+        environment: [
           'DUPLICACY_OTHER_BUCKET_B2_ID="my-id"',
           'DUPLICACY_OTHER_BUCKET_B2_KEY="my-app-key"',
         ],
@@ -206,16 +196,20 @@ export DUPLICACY_OTHER_BUCKET_B2_KEY="my-app-key"
     it { is_expected.to compile.with_all_deps }
 
     # Validate the command
-    it { is_expected.to contain_exec('init_my-repo').with_command(%r{duplicacy init -e my-repo b2://test-storage}) }
-    it { is_expected.to contain_exec('init_my-repo').with_cwd('/my/super/safe/data') }
+    it {
+      is_expected.to contain_exec('init_my-repo').with(
+        command: %r{duplicacy init -e my-repo b2://test-storage},
+        cwd: '/my/super/safe/data',
+      )
+    }
 
     # There should be a file
     it { is_expected.to have_file_resource_count(1) }
-    it { is_expected.to contain_file('env_my-repo_default').with_ensure('file') }
-    it { is_expected.to contain_file('env_my-repo_default').with_path('/my/super/safe/data/.duplicacy/puppet/scripts/default.env') }
     it {
-      is_expected.to contain_file('env_my-repo_default').with_content(
-        [
+      is_expected.to contain_file('env_my-repo_default').with(
+        ensure: 'file',
+        path: '/my/super/safe/data/.duplicacy/puppet/scripts/default.env',
+        content: [
           '#!/bin/sh
 # Export B2 Parameters
 export DUPLICACY_B2_ID="this-is-my-accound-id"
@@ -269,14 +263,12 @@ export DUPLICACY_PASSWORD="secret-sauce"
     it { is_expected.to compile.with_all_deps }
 
     # Validate the command
-    it { is_expected.to contain_exec('init_my-repo').with_command(%r{duplicacy init -e -iterations 32768 -c 4194304 -max 16777216 -min 1048576 my-repo b2://test-storage}) }
-    it { is_expected.to contain_exec('init_my-repo').with_cwd('/my/super/safe/data') }
-    it { is_expected.to contain_exec('init_my-repo').with_path('/usr/local/bin:/usr/bin:/bin') }
-
-    # Validate the environment
     it {
-      is_expected.to contain_exec('init_my-repo').with_environment(
-        [
+      is_expected.to contain_exec('init_my-repo').with(
+        command: %r{duplicacy init -e -iterations 32768 -c 4194304 -max 16777216 -min 1048576 my-repo b2://test-storage},
+        cwd: '/my/super/safe/data',
+        path: '/usr/local/bin:/usr/bin:/bin',
+        environment: [
           'DUPLICACY_PASSWORD="secret-sauce"',
           'DUPLICACY_B2_ID="this-is-my-accound-id"',
           'DUPLICACY_B2_KEY="this-is-my-key"',
@@ -312,14 +304,12 @@ export DUPLICACY_PASSWORD="secret-sauce"
     it { is_expected.to compile.with_all_deps }
 
     # Validate the command
-    it { is_expected.to contain_exec('init_my-repo').with_command(%r{duplicacy init -e -c 8388608 -max 33554432 -min 2097152 my-repo b2://test-storage}) }
-    it { is_expected.to contain_exec('init_my-repo').with_cwd('/my/super/safe/data') }
-    it { is_expected.to contain_exec('init_my-repo').with_path('/usr/local/bin:/usr/bin:/bin') }
-
-    # Validate the environment
     it {
-      is_expected.to contain_exec('init_my-repo').with_environment(
-        [
+      is_expected.to contain_exec('init_my-repo').with(
+        command: %r{duplicacy init -e -c 8388608 -max 33554432 -min 2097152 my-repo b2://test-storage},
+        cwd: '/my/super/safe/data',
+        path: '/usr/local/bin:/usr/bin:/bin',
+        environment: [
           'DUPLICACY_PASSWORD="secret-sauce"',
           'DUPLICACY_B2_ID="this-is-my-accound-id"',
           'DUPLICACY_B2_KEY="this-is-my-key"',
